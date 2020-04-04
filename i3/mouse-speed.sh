857e1ebe0ecb748e9a4a457fd5cf7afda83d4de2
Mohi Beyki <mohibeyki@gmail.com>
2020-04-04 01:52:35 -0400

    changed mouse name in xinput settings

--- i3/mouse-speed.sh
+++ i3/mouse-speed.sh
@@ -1,1 +1,1 @@
-xinput set-prop "pointer:Logitech G903" "Coordinate Transformation Matrix" 0.2 0 0 0 0.2 0 0 0 1
+xinput set-prop "pointer:Logitech Gaming Mouse G502" "Coordinate Transformation Matrix" 0.2 0 0 0 0.2 0 0 0 1

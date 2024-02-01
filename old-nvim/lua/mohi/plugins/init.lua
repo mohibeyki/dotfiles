c293bddb0a0a01e7407ecb3ea105f380d0b43d6a
Mohi Beyki <mohibeyki@gmail.com>
2024-02-01 01:47:18 -0500

    just before moving to lazy-vim

---
+++ old-nvim/lua/mohi/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+  { "tpope/vim-fugitive" },
+  { "tpope/vim-rhubarb" },
+  { "tpope/vim-sleuth" },
+  { "folke/which-key.nvim" },
+  { "numToStr/Comment.nvim" },
+  { "mbbill/undotree" },
+  { "folke/zen-mode.nvim" },
+  { "github/copilot.vim" },
+}
---
+++ old-nvim/lua/mohi/plugins/lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", config = true },
+    "williamboman/mason-lspconfig.nvim",
+    "j-hui/fidget.nvim",
+    "folke/neodev.nvim",
+  },
+}

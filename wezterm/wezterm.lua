4fcb5cf131e378ce4a13259ba6ed875ea1b7d946
Mohi Beyki <mohibeyki@gmail.com>
2024-02-13 12:31:50 -0500

    updated theme and font settings for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,13 +1,9 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.color_scheme = "Tokyo Night Storm"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 14.0
 
--- For example, changing the color scheme:
-config.color_scheme = "TokyoNight"
-
--- and finally, return the configuration to wezterm
 return config

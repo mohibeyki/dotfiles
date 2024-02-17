75db852a280b95466a3b18752ed880ecaed57169
Mohi Beyki <mohibeyki@gmail.com>
2024-02-17 00:29:58 -0500

    minor update to dotfiles

---
+++ nvim/lua/mohi/after/init.lua
@@ -0,0 +1,10 @@
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+require("lspconfig").clangd.setup({
+  on_attach = on_attach,
+  capabilities = cmp_nvim_lsp.default_capabilities(),
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+})
--- nvim/lua/mohi/init.lua
+++ nvim/lua/mohi/init.lua
@@ -5,6 +5,7 @@ function M.setup(opts)
   require("mohi.lazy")
   require("mohi.config").setup(opts)
   require("mohi.keymaps")
+  require("mohi.after")
 end
 
 return M

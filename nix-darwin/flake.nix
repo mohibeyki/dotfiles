94c5146ac2b916c4066ca8e9a687ae26c8969726
Mohi Beyki <mohibeyki@gmail.com>
2024-07-07 14:05:37 -0400

    added a default tabstop and tabshift config to neovim, added terraform and awscli to flake

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -9,77 +9,80 @@
   };
 
   outputs = { self, nix-darwin, nixpkgs, ... }@inputs:
-  let
-    configuration = { pkgs, ... }: {
-      nixpkgs.overlays = overlays;
-      environment.systemPackages =
-      [
-        pkgs.btop
-        pkgs.fd
-        pkgs.fish
-        pkgs.git
-        pkgs.gnused
-        pkgs.go
-        pkgs.gopls
-        pkgs.helix
-        pkgs.lazygit
-        pkgs.lldb
-        pkgs.lua-language-server
-        pkgs.mc
-        pkgs.neofetch
-        pkgs.neovim
-        pkgs.nodejs
-        pkgs.python3
-        pkgs.ranger
-        pkgs.ripgrep
-        pkgs.rustup
-        pkgs.selene
-        pkgs.taplo
-        pkgs.tmux
-        pkgs.wget
-        pkgs.yarn
-        pkgs.zoxide
-        pkgs.zsh
-      ];
+    let
+      configuration = { pkgs, ... }: {
+        nixpkgs.overlays = overlays;
+        environment.systemPackages =
+          [
+            pkgs.awscli2
+            pkgs.btop
+            pkgs.fd
+            pkgs.fish
+            pkgs.git
+            pkgs.gnused
+            pkgs.go
+            pkgs.gopls
+            pkgs.helix
+            pkgs.lazygit
+            pkgs.lldb
+            pkgs.lua-language-server
+            pkgs.mc
+            pkgs.neofetch
+            pkgs.neovim
+            pkgs.nodejs
+            pkgs.python3
+            pkgs.ranger
+            pkgs.ripgrep
+            pkgs.rustup
+            pkgs.selene
+            pkgs.taplo
+            pkgs.terraform
+            pkgs.tmux
+            pkgs.wget
+            pkgs.yarn
+            pkgs.zoxide
+            pkgs.zsh
+          ];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      nix.package = pkgs.nix;
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+        nixpkgs.config.allowUnfree = true;
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;
-      programs.fish.enable = true;
-      programs.fish.shellInit = ''
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;
+        programs.fish.enable = true;
+        programs.fish.shellInit = ''
         for p in /run/current-system/sw/bin
           if not contains $p $fish_user_paths
             set -g fish_user_paths $p $fish_user_paths
           end
         end
-      '';
+        '';
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 4;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+      overlays = [
+        inputs.neovim-nightly-overlay.overlays.default
+      ];
+    in
+      {
+      darwinConfigurations."Legolas" = nix-darwin.lib.darwinSystem {
+        modules = [ configuration ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."Legolas".pkgs;
     };
-    overlays = [
-      inputs.neovim-nightly-overlay.overlays.default
-    ];
-  in
-  {
-    darwinConfigurations."Legolas" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."Legolas".pkgs;
-  };
 }
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -56,3 +56,7 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true

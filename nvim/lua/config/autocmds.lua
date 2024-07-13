8434d1fedf30ee93f3a9e3e6d7ba8e03fe3732ce
Mohi Beyki <mohibeyki@gmail.com>
2024-07-13 12:34:42 -0400

    updated configuration for which-key v3 update, added autocmd to update neovim ui after resizing its window/panels

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,21 @@
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking text',
-    group = vim.api.nvim_create_augroup('augroup-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
 })
--- nvim/lua/plugins/ui/which-key.lua
+++ nvim/lua/plugins/ui/which-key.lua
@@ -1,24 +1,50 @@
 return {
-    -- Useful plugin to show you pending keybinds.
+    -- which-key helps you remember key bindings by showing a popup
+    -- with the active keybindings of the command you started typing.
     {
-        'folke/which-key.nvim',
-        event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-        config = function() -- This is the function that runs, AFTER loading
-            require('which-key').setup()
-            -- Document existing key chains
-            require('which-key').register {
-                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-                ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-                ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-            }
-            -- visual mode
-            require('which-key').register({
-                ['<leader>h'] = { 'Git [H]unk' },
-            }, { mode = 'v' })
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts_extend = { "spec" },
+        opts = {
+            defaults = {},
+            spec = {
+                {
+                    mode = { "n", "v" },
+                    { "<leader><tab>", group = "tabs" },
+                    { "<leader>b",     group = "buffer" },
+                    { "<leader>c",     group = "code" },
+                    { "<leader>f",     group = "file/find" },
+                    { "<leader>g",     group = "git" },
+                    { "<leader>gh",    group = "hunks" },
+                    { "<leader>q",     group = "quit/session" },
+                    { "<leader>s",     group = "search" },
+                    { "<leader>u",     group = "ui" },
+                    { "<leader>w",     group = "windows" },
+                    { "<leader>x",     group = "diagnostics/quickfix" },
+                    { "[",             group = "prev" },
+                    { "]",             group = "next" },
+                    { "g",             group = "goto" },
+                    { "gs",            group = "surround" },
+                    { "z",             group = "fold" },
+                },
+            },
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+        config = function(_, opts)
+            local wk = require("which-key")
+            wk.setup(opts)
+            if not vim.tbl_isempty(opts.defaults) then
+                LazyVim.warn("which-key: opts.defaults is deprecated. Please use opts.spec instead.")
+                wk.register(opts.defaults)
+            end
         end,
     },
 }

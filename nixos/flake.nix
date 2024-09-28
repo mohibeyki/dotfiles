3a2fe33c3e55d7c365aab4a0b10361634f0564f3
Mohi Beyki <mohibeyki@gmail.com>
2024-09-28 15:54:29 -0400

    WIP, added hyprland and configured it, trying to fix brave flickering

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,13 +4,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nix-darwin, ... } @ inputs:
+  outputs = { self, nixpkgs, nix-darwin, lanzaboote, ... } @ inputs:
     {
       nixosConfigurations = {
         sauron = nixpkgs.lib.nixosSystem {
@@ -19,9 +24,24 @@
             inherit inputs;
           };
           modules = [
+            lanzaboote.nixosModules.lanzaboote
             ./hosts/sauron/configuration.nix
             ./modules/common/common.nix
             ./modules/nixos/nixos.nix
+
+            ({ pkgs, lib, ... }: {
+
+              environment.systemPackages = [
+                pkgs.sbctl
+              ];
+
+              boot.loader.systemd-boot.enable = lib.mkForce false;
+
+              boot.lanzaboote = {
+                enable = true;
+                pkiBundle = "/etc/secureboot";
+              };
+            })
           ];
         };
       };
--- nixos/modules/nixos/nixos.nix
+++ nixos/modules/nixos/nixos.nix
@@ -1,12 +1,58 @@
 { pkgs, ... }:
 {
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSOR = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            leftmeta = "oneshot(alt)";
+            leftalt = "oneshot(meta)";
+          };
+        };
+      };
+    };
+  };
+
+  nixpkgs.config.overlays = [
+    (self: super: {
+      brave = super.brave.override {
+        commandLineArgs = "--password-store=gnome --disable-gpu-compositing";
+      };
+    })
+  ];
+
   environment.systemPackages = with pkgs; [
     alacritty
     brave
     discord
     gparted
+    hyprpaper
+    mako
+    niv
+    sbctl
     vimix-cursors
+    waybar
     wezterm
+    wofi
+    zed-editor
+
+    (pkgs.waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ]; } ) )
   ];
 }
-

6123c9b238f8988a7a69801f2788faa807a105eb
Mohi Beyki <mohibeyki@gmail.com>
2024-10-04 23:20:18 -0400

    switched default term in hyprland to wezterm, minor change to wezterm settings to fix weird border's issue

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,21 +3,22 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 16.0
+config.font_size = 14.0
 
-config.window_decorations = "RESIZE"
-config.window_close_confirmation = "AlwaysPrompt"
+config.window_decorations = "TITLE | RESIZE"
+config.window_close_confirmation = "NeverPrompt"
 config.scrollback_lines = 3000
+config.enable_scroll_bar = true
 config.default_workspace = "home"
 config.native_macos_fullscreen_mode = true
 config.hide_tab_bar_if_only_one_tab = true
 
 config.keys = {
-  {
-    key = "Enter",
-    mods = "CMD",
-    action = wezterm.action.ToggleFullScreen,
-  },
+	{
+		key = "Enter",
+		mods = "CMD",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 
 return config

1052815b9e005ec33f400b53076846341c121b29
Mohi Beyki <mohibeyki@gmail.com>
2024-10-07 13:21:30 -0400

    minor update to wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,7 +12,15 @@ config.scrollback_lines = 3000
 config.enable_scroll_bar = true
 config.default_workspace = "home"
 config.native_macos_fullscreen_mode = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+-- Tabs!
+config.inactive_pane_hsb = {
+  saturation = 0.25,
+  brightness = 0.5,
+}
 
 config.leader = { key = "a", mods = "CTRL" }
 config.keys = {
@@ -45,6 +53,71 @@ config.keys = {
   { key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
   { key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
   { key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+  { key = "[", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+  { key = "]", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+  { key = "t", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
 }
+
+-- Status bar
+config.status_update_interval = 1000
+
+wezterm.on("update-status", function(window, pane)
+  -- Workspace name
+  local stat = window:active_workspace()
+  local stat_color = "#f7768e"
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then
+    stat = window:active_key_table()
+    stat_color = "#7dcfff"
+  end
+  if window:leader_is_active() then
+    stat = "LDR"
+    stat_color = "#bb9af7"
+  end
+
+  local basename = function(s)
+    -- Nothing a little regex can't fix
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+  end
+
+  -- Current working directory
+  local cwd = pane:get_current_working_dir()
+  if cwd then
+    cwd = basename(cwd)
+  else
+    cwd = ""
+  end
+
+  -- Current command
+  local cmd = pane:get_foreground_process_name()
+  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+  cmd = cmd and basename(cmd) or ""
+
+  -- Time
+  local time = wezterm.strftime("%H:%M")
+
+  -- Left status (left of the tab line)
+  window:set_left_status(wezterm.format({
+    { Foreground = { Color = stat_color } },
+    { Text = "  " },
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+    { Text = " |" },
+  }))
+
+  -- Right status
+  window:set_right_status(wezterm.format({
+    -- Wezterm has a built-in nerd fonts
+    -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+    { Text = " | " },
+    { Foreground = { Color = "#e0af68" } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+    "ResetAttributes",
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+    { Text = "  " },
+  }))
+end)
 
 return config

c966528aca930f8d9b471c194ea5c5c5a5777721
Mohi Beyki <mohibeyki@gmail.com>
2024-10-07 21:46:15 -0400

    attempt to fix hyprland without the overlay

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -21,9 +21,6 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage =
-      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   environment.systemPackages = [

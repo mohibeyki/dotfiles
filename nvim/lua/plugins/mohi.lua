36c1070b64c6ea992fbfe8dc7d25620cc8aae9cf
Mohi Beyki <mohibeyki@gmail.com>
2025-03-02 13:27:47 -0800

    switched from nil_ls to nixd

--- nvim/lua/plugins/mohi.lua
+++ nvim/lua/plugins/mohi.lua
@@ -1,1 +1,23 @@
-return {}
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "nix" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nixd = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+}

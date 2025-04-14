d7d0edd78b80de10d4a71fd6374eb7e238d29455
Mohi Beyki <mohibeyki@gmail.com>
2025-04-14 01:47:13 -0700

    updated nix-darwin repo

--- nix/flake.nix
+++ nix/flake.nix
@@ -17,7 +17,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

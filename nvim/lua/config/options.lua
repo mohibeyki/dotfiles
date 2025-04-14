1b348a1b692af2c7c956cb9d49b1491fbae9ba97
Mohi Beyki <mohi@meta.com>
2025-04-14 10:02:13 -0700

    updated meta plugin logic, updated nvim packages

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,10 +3,3 @@
 -- Add any additional options here
 
 vim.g.lazyvim_check_order = false
-
-vim.g.meta = false
-local f = io.open("/usr/share/fb-editor-support", "r")
-if f ~= nil then
-  io.close(f)
-  vim.g.meta = true
-end

7d81f24c4875056d889a427c3816abab80f9f195
Mohi Beyki <mohibeyki@gmail.com>
2025-11-12 23:40:25 -0800

    updated lazyvim

--- config/nvim/lua/plugins/mohi.lua
+++ config/nvim/lua/plugins/mohi.lua
@@ -1,21 +1,2 @@
 return {
-  { "nvim-mini/mini.pairs", enabled = false },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = { ensure_installed = { "nix" } },
-  },
-  {
-    dir = "/usr/share/fb-editor-support/nvim",
-    name = "meta.nvim",
-    cond = function()
-      local file = vim.fn.expand("/usr/share/fb-editor-support/nvim")
-      local f = io.open(file, "r")
-      if f ~= nil then
-        io.close(f)
-        return true
-      else
-        return false
-      end
-    end,
-  },
 }

5c28af37a24379d8a572331b78d91bd76b28d22f
Mohi Beyki <mohibeyki@gmail.com>
2026-01-14 22:44:33 -0800

    Updated nixos configuration for resolved

--- nix/modules/nixos.nix
+++ nix/modules/nixos.nix
@@ -52,13 +52,18 @@
 
     resolved = {
       enable = true;
-      dnssec = "true";
-      domains = [ "~." ];
-      fallbackDns = [
-        "1.1.1.1#one.one.one.one"
-        "1.0.0.1#one.one.one.one"
-      ];
-      dnsovertls = "true";
+
+      settings = {
+        Resolve = {
+          DNSSEC = "true";
+          DNSOverTLS = true;
+          Domains = [ "~." ];
+          FallbackDNS = [
+            "1.1.1.1#one.one.one.one"
+            "1.0.0.1#one.one.one.one"
+          ];
+        };
+      };
     };
 
     # Disable CUPS

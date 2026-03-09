50dc6d11f96fcc787b190bf68cf873c01b548f33
Mohi Beyki <mohibeyki@gmail.com>
2026-03-09 01:39:05 -0700

    small update to wallpaper and its location

--- nix/flake.nix
+++ nix/flake.nix
@@ -105,19 +105,19 @@
             }
           ];
           workspaces = [
-            "1, monitor:DP-4"
-            "2, monitor:DP-5"
-            "3, monitor:DP-4"
-            "4, monitor:DP-5"
-            "5, monitor:DP-4"
-            "6, monitor:DP-5"
-            "7, monitor:DP-4"
-            "8, monitor:DP-5"
-            "9, monitor:DP-4"
-            "10, monitor:DP-5"
+            "1, monitor:DP-1"
+            "2, monitor:DP-2"
+            "3, monitor:DP-1"
+            "4, monitor:DP-2"
+            "5, monitor:DP-1"
+            "6, monitor:DP-2"
+            "7, monitor:DP-1"
+            "8, monitor:DP-2"
+            "9, monitor:DP-1"
+            "10, monitor:DP-2"
           ];
           primaryMonitor = "DP-5";
-          wallpaper = "~/Pictures/sunset.jpg";
+          wallpaper = "~/Pictures/Wallpapers/sunset.jpg";
           nvidia = true;
         }
         [

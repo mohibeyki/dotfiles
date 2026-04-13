a36715f7e8487838831ed0c494601561900e6372
Mohi Beyki <mohibeyki@gmail.com>
2026-04-12 23:04:18 -0700

    fixed rootless docker dns settings on sauron

--- nixos-modules/containers.nix
+++ nixos-modules/containers.nix
@@ -3,6 +3,13 @@
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
+    daemon.settings = {
+      dns = [
+        "192.168.1.10"
+        "1.1.1.1"
+        "8.8.8.8"
+      ];
+    };
   };
 
   environment.systemPackages = with pkgs; [

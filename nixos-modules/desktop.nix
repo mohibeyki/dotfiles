0f15e3a03efa1708207e17b057b8a206eda423b1
Mohi Beyki <mohibeyki@gmail.com>
2026-09-02 23:07:00 -0700

    Allow 1Password to unlock the Vivaldi browser extension

--- nixos-modules/desktop.nix
+++ nixos-modules/desktop.nix
@@ -13,6 +13,15 @@
     };
   };
 
+  # Allow the 1Password desktop app to unlock the Vivaldi extension.
+  # https://wiki.nixos.org/wiki/1Password#Unlocking_browser_extensions
+  environment.etc."1password/custom_allowed_browsers" = {
+    text = ''
+      vivaldi-bin
+    '';
+    mode = "0755";
+  };
+
   services = {
     desktopManager.plasma6.enable = true;
     blueman = {

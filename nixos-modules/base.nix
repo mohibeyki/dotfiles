3fb4727ecff82d8582f9d74f2e49da29af5342a1
Mohi Beyki <mohibeyki@gmail.com>
2026-09-11 19:22:35 -0700

    Remove unavailable CUDA binary cache

--- nixos-modules/base.nix
+++ nixos-modules/base.nix
@@ -40,12 +40,10 @@
   nix.settings = {
     extra-substituters = [
       "https://hyprland.cachix.org"
-      "https://cuda-maintainers.cachix.org"
       "https://nix-gaming.cachix.org"
     ];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
